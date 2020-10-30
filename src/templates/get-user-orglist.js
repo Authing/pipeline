3c46ec02b9177f691ece92bfc949fb8bdd238062
cj <cj@authing.cn>
2020-10-30 21:19:01 +0800

    feat: Get User Org List

---
+++ src/templates/get-user-orglist.js
@@ -0,0 +1,23 @@
+
+/**
+ * @title_zh 获取用户组织机构列表
+ * @title_en Get User Org List
+ * @type POST_REGISTER,POST_AUTHENTICATION
+ * @category 扩展用户字段
+ * @category_en Extend UserInfo
+ * @overview_zh 获取用户所在的组织机构列表
+ * @overview_en Get User Org List
+ */
+
+async function pipe(user, context, callback) {
+  const userId = user.id;
+  const orgList = await authing.users.listOrgs(userId)
+  // 你可以使用 log 方法查看该函数的 
+  // log(data)
+  const orgIds = []
+  for (const nodePath of orgList) {
+    orgIds.push(nodePath.find(item => item.type === 'org')?.id)
+  }
+  data.addToken('orgIds', orgIds)
+  callback(null, user, context)
+}
